9c4e28d30cfe5d9888c26cb1cf2b8b30d997c2ee
Xthe23 <michaelkravitz98@gmail.com>
2023-12-04 23:34:50 -0500

    Updated: app.py to include a more descriptive home route, and screenshots into Resources folder.

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -42,8 +42,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start year<br/>"
+        f"/api/v1.0/start year/end year<br/>"
     )
 
 
